30857e325f985f1bf3de1fc49fca159667b0cfda
Maxun <trukhinmaksimof@mail.ru>
2023-08-01 02:14:55 +0800

    writing logic for open 'Confirmation' component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,26 @@
-import React, { memo, useRef } from 'react'
+import React, { memo, useRef, useEffect } from 'react'
 import Navbar from './Navbar'
 import {Outlet} from "react-router-dom"
 import Modal from './UI/Components/Modal/Modal'
 import ModalContext from './Context/modalContext'
+import Confirmation from './UI/Components/Confirmation/Confirmation'
 
 export default memo(function Layout() {
   const modalRef = useRef(null);
-  
+  const confirmationRef = useRef(null);
+
+  useEffect(() => {
+    //confirmationRef.current.setText("Asdfg")
+    const f = () => confirmationRef.current.show("Asdfgh")
+    document.addEventListener("click", f)
+    return document.removeEventListener("click", f)
+  }, [])
+
   return (
     <>
         <Modal ref = {modalRef} />
-        <ModalContext.Provider value = {{modalRef : modalRef}}>
+        <Confirmation ref = {confirmationRef} />
+        <ModalContext.Provider value = {{modalRef : modalRef, confirmationRef}}>
           <Navbar />
           <Outlet/>
         </ModalContext.Provider>
--- src/UI/Components/Confirmation/Confirmation.jsx
+++ src/UI/Components/Confirmation/Confirmation.jsx
@@ -9,27 +9,29 @@ export default forwardRef(function (props, ref) {
     const [text, setText] = useState("Qwerty")
     const confirmBtnRef = useRef(null)
     const rejectBtnRef = useRef(null)
+    const pRef = useRef(null)
 
     useImperativeHandle(ref, () => {
         return {
-            show : (text) => {
-                setText(text)
+            setText,
+            show : (request) => {
+                pRef.current.innerHTML = "asdfg"//request.toString()
                 dialogRef.current.showModal()
                 return new Promise((resolve, reject) => {
                     confirmBtnRef.current.onClick = () => resolve("OK")
-                    rejectBtnRef.current.onClick = () => reject("OK")
+                    rejectBtnRef.current.onClick = () => reject("NO")
                 })
             }
         }
-    })
+    }, [])
 
     return (
         <dialog className={style["confirmation_dialog"]} ref = {dialogRef} open = {true}>
             <form method = "dialog">
                 <h2>Confirm action</h2>
-                <p>{text}</p>
-                <button ref = {confirmBtnRef} className = {styled_buttons["success-btn"]} type = "submit">Confirm</button>
-                <button ref = {rejectBtnRef} className = {styled_buttons["warning-btn"]} type = "button">Reject</button>
+                <p ref = {pRef}></p>
+                <button ref = {confirmBtnRef} className = {styled_buttons["success-btn"]}>Confirm</button>
+                <button ref = {rejectBtnRef} className = {styled_buttons["warning-btn"]}>Reject</button>
             </form>
         </dialog>
     )
