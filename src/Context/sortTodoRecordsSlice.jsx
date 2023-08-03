c88bff97dc2219e9eb6cd3836c99df751e0cbddb
Maxun <trukhinmaksimof@mail.ru>
2023-08-04 03:16:22 +0800

    prepared slice for sorting parameters

---
+++ src/Context/sortTodoRecordsSlice.jsx
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const sortTodoRecordsSlice = createSlice({
+    name : "sortTodoRecordsSlice",
+    initialState : {
+        __proto__ : null,
+        parameter : null,
+        reverse : false
+    },
+    reducers : {
+        setSortParams : (state, action) => {
+            return {
+                parameter : action.payload.parameter,
+                reverse : action.payload.reverse
+            }
+        },
+        resetSortParams : () => {
+            return {
+                __proto__ : null,
+                parameter : null,
+                reverse : false
+            }
+        }
+    }
+})
+
+export const {setSortParams, resetSortParams} = sortTodoRecordsSlice.actions
+
+export default sortTodoRecordsSlice.reducer
