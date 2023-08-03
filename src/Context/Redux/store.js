d6ffc2af299525212f45d991f42e8ca4050ce46b
Maxun <trukhinmaksimof@mail.ru>
2023-08-04 03:19:31 +0800

    merged with dev

--- src/Context/Redux/store.js
+++ src/Context/Redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import todoRecordsReducer from "./todoRecordsSlice";
 import todoRecordsCollectionReducer from "./todoCollectionsSlice";
 import filtersSliceReducer from "./filterTodoRecordsSlice"
+import sortTodoRecordsReducer from "../sortTodoRecordsSlice";
 
 export const store = configureStore({
     reducer : {
-       "todoRecords" : todoRecordsReducer,
-       "todoRecordsCollection" : todoRecordsCollectionReducer,
-       "filterTodoRecords" : filtersSliceReducer,
+        "todoRecords" : todoRecordsReducer,
+        "todoRecordsCollection" : todoRecordsCollectionReducer,
+        "filterTodoRecords" : filtersSliceReducer,
+        "sortTodoRecords" : sortTodoRecordsReducer
     }
 })
