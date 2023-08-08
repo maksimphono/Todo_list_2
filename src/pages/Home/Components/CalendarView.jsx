7d61e17c2e5bd800ba366a8c29fa19a7e2d7bb1e
Maxun <trukhinmaksimof@mail.ru>
2023-08-09 03:12:50 +0800

    created function, that fills array with all days in specified month of specified year

--- src/pages/Home/Components/CalendarView.jsx
+++ src/pages/Home/Components/CalendarView.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import style from "../styles/Calendar.module.scss"
 
@@ -10,8 +10,44 @@ Array.range = (start, stop, step = 1) => {
     })())
 }
 
+function fillMonth(year, month) {
+    let monthAsTable = []
+
+    const daysInMonths = new Map([
+            [1, 31],
+            [2, (new Date(`${year}-02-29`) != "Invalid Date")?29:28],
+            [3, 31],
+            [4, 30],
+            [5, 31],
+            [6, 30],
+            [7, 31],
+            [8, 31],
+            [9, 30],
+            [10, 31],
+            [11, 30],
+            [12, 31]
+    ])
+
+    const firstDay = new Date(`${year}-${month}-1`)
+    const firstDayOfWeek = firstDay.getDay()
+    const prevMonth = (month - 1) || 12
+    
+    for (let day = daysInMonths.get(prevMonth) - firstDayOfWeek + 1; day <= daysInMonths.get(prevMonth); day++) {
+        monthAsTable.push(day)
+    }
+    monthAsTable = [...monthAsTable, ...Array.range(1, daysInMonths.get(month) + 1)]
+    
+    monthAsTable = [...monthAsTable, ...Array.range(1, 42 - monthAsTable.length + 1)]
+
+    return monthAsTable
+}
+
 export default function CalendarView() {
-  return (
+  
+    useEffect(() => console.table(fillMonth(2022, 7)), [])
+    
+    
+    return (
     <>
         <table className={style["calendar"]}>
             <button></button>
---
+++ src/pages/Home/Components/TodoCardsByDay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import style from "../styles/TodoCardByDay.module.scss"
+
+export default function TodoCardsByDay() {
+    return (
+        <div className = {style["todo_cards_by_day"]}>
+            
+        </div>
+    )
+}
