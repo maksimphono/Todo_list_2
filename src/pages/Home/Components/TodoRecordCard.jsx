6d9acd73c84f5a953904fbba5e2d2abc6ff71f4b
Maxun <trukhinmaksimof@mail.ru>
2023-08-22 21:34:02 +0700

    added svg background

--- src/pages/Home/Components/TodoRecordCard.jsx
+++ src/pages/Home/Components/TodoRecordCard.jsx
@@ -83,6 +83,7 @@ export default function TodoRecordCard({cardData}) {
           style = {{"--bg-main-color" : todoCollection.color, "--text-color" : textColor}}
           className = {style["todo-record-card"]}
         >
+            <svg className = {style["background_down"]} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,160L40,149.3C80,139,160,117,240,133.3C320,149,400,203,480,192C560,181,640,107,720,101.3C800,96,880,160,960,192C1040,224,1120,224,1200,202.7C1280,181,1360,139,1400,117.3L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
             <h3 className ={style["title"]}>{newCardData.title}</h3>
             <DateStamp date = {newCardData.dateEnd}/>
             <span className ={style["type"]}>Collection "<b>{todoCollection.name}</b>"</span>
