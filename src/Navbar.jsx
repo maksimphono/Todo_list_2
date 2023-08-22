6872064dd87a857ff6ac76edfc167bddfff7e6ea
Maxun <trukhinmaksimof@mail.ru>
2023-08-23 00:13:48 +0700

    changed style of <a> elements to display icons more properly

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,18 @@
 //import "../define.scss"
-import style from "./navbar.scss";
+import style from "./navbar.module.scss";
 import React from 'react'
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav id = "navbar">
-        <div className = "brand">
+    <nav id = {style["navbar"]}>
+        <div className = {style["brand"]}>
             <Link to="/" />
         </div>
-        <button className = "navbar-toggler"></button>
+        <button className = {style["navbar-toggler"]}></button>
         <ul>
-            <li><Link to="/"><span class="material-symbols-outlined">done</span>Home</Link></li>
-            <li><Link to="/NewTodoRecord">New</Link></li>
+            <li><Link to="/"><span className="material-symbols-outlined">home</span> Home</Link></li>
+            <li><Link to="/NewTodoRecord"><span className="material-symbols-outlined">add</span> New</Link></li>
             <li><Link to="/">Analize</Link></li>
             <button>P</button>
         </ul>            
