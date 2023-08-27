dab0fd100250f1ca6db2e6bf619d9b4fc977d98b
Maxun <trukhinmaksimof@mail.ru>
2023-08-28 02:43:19 +0700

    deploy on Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ function App() {
   return (
     <Provider store = {store}>
     
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path = "/" element = {<Layout />}>
           <Route index element = {<TodoRecordsList />}/>
@@ -91,7 +91,7 @@ function App() {
           <Route index element = {<CalendarView/>}/>
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </Provider>
   )
 }
