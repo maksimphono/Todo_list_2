4649a2f8e26313992781c7f85873132e45202cc9
Maxun <trukhinmaksimof@mail.ru>
2024-01-26 01:38:07 +0700

    successfuly created method "saveOne" of "DataAdapter" class, added one entry to the indexedDB

--- src/IndexedDB/DataAdapter.js
+++ src/IndexedDB/DataAdapter.js
@@ -2,7 +2,7 @@ class DataAdapter{
     constructor(dbStoreName, fields, dbName, version) {
         this.name = dbStoreName
         this.version = version
-        this.fields = [...fields]
+        this.fields = fields
       	this.db = null
       	this.dbName = dbName
     }
@@ -16,6 +16,7 @@ class DataAdapter{
     		req.onerror = event => onErr()
     		req.onupgradeneeded = event => {
         		let store = event.currentTarget.result.createObjectStore(this.name, {keyPath : "id"})
+            console.table(this.fields)
           	this.fields.forEach(field => store.createIndex(field.name, field.name, {unique : field.unique}) )
         }
 		}
@@ -23,15 +24,31 @@ class DataAdapter{
         let tx = this.db.transaction(store_name, mode)
         return tx.objectStore(store_name)
     }
+    async saveOne(entry) {
+        let obj = new Object()
+        this.fields.forEach(field => obj[field.name] = entry[field.name])
+        const store = this.getObjectStore(this.name, "readwrite")
+        let req = null
+        console.dir(obj)
+        try {
+            req = store.add(obj)
+        } catch(error) {
+            console.error(error)
+            throw error
+        }
+        req.onsuccess = () => console.log("Added")
+        req.onerror = () => console.log("Error adding")
+    }
   
 }
 
 async function test() {
   	const dbName = "TodoDatabase"
     const fields = [{name:"id", unique : true}, {name:"Col1", unique : false}, {name:"COl2", unique : false}]
-    let da = new DataAdapter("My store", fields, dbName, 1)
+    let da = new DataAdapter("My store", fields, dbName, 2)
     let p = new Promise((res, rej) => da.openDB(res, rej))
     await p
     console.log(da.getObjectStore("My store", "readwrite"))
+  	da.saveOne({"id" : "1", "Col1" : "Qwertyu", "COl2" : "zxcvbn"})
 }
 test()
